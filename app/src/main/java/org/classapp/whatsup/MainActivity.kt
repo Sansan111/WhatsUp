cedf6257c5cd1d5ce1204e2f9180f7f2d8f0b063
Sankrub <navathon.l@ku.th>
2025-02-01 14:39:24 +0700

    First commit WhatsUp app

---
+++ app/src/main/java/org/classapp/whatsup/MainActivity.kt
@@ -0,0 +1,51 @@
+package org.classapp.whatsup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.classapp.whatsup.ui.theme.WhatsUpTheme
+import androidx.compose.material3.Button
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WhatsUpTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+    Button(onClick = {}) {
+        Text(text = "Click Me")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WhatsUpTheme {
+        Greeting("Android")
+    }
+}
