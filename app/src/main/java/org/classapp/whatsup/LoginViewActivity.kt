806a60209e74a568d0807203fc9317b37c6ccbde
Sansan111 <shannavathon@gmail.com>
2025-02-03 23:28:53 +0700

    Finish all the task

---
+++ app/src/main/java/org/classapp/whatsup/LoginViewActivity.kt
@@ -0,0 +1,50 @@
+package org.classapp.whatsup
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val signInBtn: Button = findViewById(R.id.signInBtn)
+        val signInPanel: CardView = findViewById(R.id.signInPanel)
+        val usernameTxt:EditText = findViewById(R.id.usernameTxt)
+        val pwdTxt:EditText = findViewById(R.id.pwdTxt)
+        val enterBtn:Button = findViewById(R.id.enterBtn)
+        signInBtn.setOnClickListener { signInPanel.visibility = View.VISIBLE }
+
+        var username:String; var password:String
+        enterBtn.setOnClickListener {
+             username = usernameTxt.text.toString()
+             password = pwdTxt.text.toString()
+
+            if (username.equals("android")  && password.equals("google"))
+            {
+                Toast.makeText(this, "Welcome to WhatsUp!!", Toast.LENGTH_LONG).show()
+                usernameTxt.setText("")
+                pwdTxt.setText("")
+                signInPanel.visibility = View.GONE
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+            } else {
+                Toast.makeText(this, "Please Try Again", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
--- app/src/main/java/org/classapp/whatsup/MainActivity.kt
+++ app/src/main/java/org/classapp/whatsup/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.classapp.whatsup
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,19 +16,83 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.classapp.whatsup.ui.theme.WhatsUpTheme
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavGraph.Companion.findStartDestination
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             WhatsUpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.wrapContentSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    MainScreenWithBottomNavbar()
                 }
+            }
+        }
+        Toast.makeText(this, "Welcome to WhatsUp!!", Toast.LENGTH_LONG).show()
+    }
+}
+
+@Composable
+fun MainScreenWithBottomNavbar() {
+    val navController = rememberNavController()
+    var navSelectedItem by remember {
+        mutableStateOf(0)
+    }
+    Scaffold(bottomBar = {
+        NavigationBar {
+            WhatsUpNavItemInfo().getAllNavItems().forEachIndexed { index, itemInfo ->
+                NavigationBarItem(selected = (index==navSelectedItem),
+                    onClick = {
+                        navSelectedItem = index
+                        navController.navigate(itemInfo.route) {
+                            popUpTo(navController.graph.findStartDestination().id){
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = itemInfo.icon,
+                            contentDescription = itemInfo.label
+                        )
+                    },
+                    label = { Text(text = itemInfo.label) })
+
+            }
+        }
+
+    }) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = DestinationScreens.NearMe.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(route = DestinationScreens.Highlight.route) {
+                HighlightScreen()
+            }
+            composable(route = DestinationScreens.NearMe.route) {
+                NearMeScreen()
+            }
+            composable(route = DestinationScreens.MyEvents.route) {
+                MyEventsScreen()
             }
         }
     }
@@ -47,5 +114,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     WhatsUpTheme {
         Greeting("Android")
+        MainScreenWithBottomNavbar()
     }
 }
+
+
+
+
